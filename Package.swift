410b82e193592246eedf7ef9940cb979f62a7311
Stanislav Ageev <stanislav@roomd.net>
2024-12-04 13:57:54 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -32,13 +32,13 @@ let package = Package(
         ),
         .target(
             name: "FlagPhoneNumber",
-            resources: [
-                .process("Resources")
-            ]
             dependencies: ["libPhoneNumber"],
             path: "Sources",
             exclude: ["Info.plist", "libPhoneNumber"],
-            publicHeadersPath: "."
+            publicHeadersPath: ".",
+            resources: [
+                .process("Resources")
+            ]
         ),
     ]
 )
